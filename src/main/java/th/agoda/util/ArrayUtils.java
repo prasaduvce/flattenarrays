5e96b3627fad0749f3bca56dfb4d17020fc82988
renuka.prasad <renuka.prasad@rakuten.com>
2017-04-26 15:28:14 +0900

    Code warnings fixed.

--- src/main/java/th/agoda/util/ArrayUtils.java
+++ src/main/java/th/agoda/util/ArrayUtils.java
@@ -3,20 +3,19 @@ package th.agoda.util;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ArrayUtils {
+class ArrayUtils {
 
 	private List<Integer> finalOutputList = new ArrayList<Integer>();
 
-	public Integer[] flattenArray(Object[] inputArray) {
+	Integer[] flattenArray(Object[] inputArray) {
 		if (inputArray == null) {
 			return new Integer[0];
 		}
-		for (int i=0; i<inputArray.length; i++) {
-			Object obj = inputArray[i];
+		for (Object obj : inputArray) {
 			if (Integer.class.equals(obj.getClass())) {
 				finalOutputList.add((Integer) obj);
 			} else if (Object[].class.equals(obj.getClass())) {
-				flattenArray((Object[])obj);
+				flattenArray((Object[]) obj);
 			} else {
 				throw new RuntimeException("Not able to flatten the array, due to not matching type");
 			}
